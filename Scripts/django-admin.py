33064a0de964ab3909dc29558cc47c0b522cd71c
bratakhtania <tanitabra20@gmail.com>
2020-07-13 10:01:01 +0300

    initial project template

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\appdata\local\programs\django_project\proect1\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
